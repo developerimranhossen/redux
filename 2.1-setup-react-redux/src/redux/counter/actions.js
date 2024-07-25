df557194295b71e44b2ad067e784d83bbdb7fb5e
imran <developerimranhossen@gmail.com>
2024-07-26 02:07:59 +0600

    add

---
+++ 2.1-setup-react-redux/src/redux/counter/actions.js
@@ -0,0 +1,15 @@
+// here action creator
+import { DECREMENT, INCREMENT } from "./actionTypes";
+export const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+
+export const decrement = (value) => {
+    return {
+      type: DECREMENT,
+      payload: value,
+    };
+  };
